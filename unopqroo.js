b976e8947207427f222c8850685af4ae57956650
kevincaamal11 <kevincaamal70@gmail.com>
2023-10-17 18:15:50 -0500

    progreso

--- unopqroo.js
+++ unopqroo.js
@@ -1,67 +1,80 @@
-
 const rl = require("readline-sync");
-//juego uno
-var deck = [];
-var colors = ["Yellow", "Blue", "Green", "Red"];
-var type =["Comun", "Especial", "Comodin"];
+// juego uno 
+var  deck = [];
+var Colors = ["Yellow","Blue", "Green","Red"];
+var type = ["Comun","Especial","Comodin"];
 var card = {
-    color: null, //amarillo, azul, verde y rojjo
-    number: null,// 0-9
-    type: null// comun, especial y comodines
+    color: null, // el color puede ser amarillo, azul, verde y rojo
+    number: null,  // del 0 al 9
+    type: null // comun, especial y comodin
 };
 
-for (var cColor = 0; cColor < colors.length; cColor++) {
-    
-    for(var cNumber = 0; cNumber < 10; cNumber++){
-        deck.push({color: colors[cColor], number:cNumber, typ:"comun"});
-
+for (var cColor = 0; cColor < Colors.length; cColor++ ){
+    for (var cNumber= 0; cNumber < 10; cNumber++){
+        deck.push({Color: Colors[cColor], Number: cNumber, type:"comun"});
     }
-    for(var cNumber1= 1; cNumber1 < 10; cNumber1++){
-        deck.push({color: colors[cColor], number:cNumber1, type:"comun"});
-       
+    for (var cNumber1= 1; cNumber1 < 10; cNumber1++){
+        deck.push({Color: Colors[cColor], Number1: cNumber1, type:"comun"});
     }
 
-    deck.push({color: colors[cColor], number: "+2", typ:"especial"});
-    deck.push({color: colors[cColor], number: "+2", typ:"especial"});
-    deck.push({color: colors[cColor], number: "B", typ:"especial"});
-    deck.push({color: colors[cColor], number: "B", typ:"especial"});
-    deck.push({color: colors[cColor], number: "R", typ:"especial"});
-    deck.push({color: colors[cColor], number: "R", typ:"especial"});
-
+deck.push({color: Colors [cColor],number:"+2", type:"especial"});
+deck.push({color: Colors [cColor],number:"+2", type:"especial"});
+deck.push({color: Colors [cColor],number:"B", type:"especial"});
+deck.push({color: Colors [cColor],number:"B", type:"especial"});
+deck.push({color: Colors [cColor],number:"R", type:"especial"});
+deck.push({color: Colors [cColor],number:"R", type:"especial"});
 }
-
-    deck.push({color: null, number: "+4", typ:"comodin"});
-    deck.push({color: null, number: "+4", typ:"comodin"});
-    deck.push({color: null, number: "+4", typ:"comodin"});
-    deck.push({color: null, number: "+4", typ:"comodin"});
-    deck.push({color: null, number: "cc", typ:"comodin"});
-    deck.push({color: null, number: "cc", typ:"comodin"});
-    deck.push({color: null, number: "cc", typ:"comodin"});
-    deck.push({color: null, number: "cc", typ:"comodin"});
-
+deck.push({color: null,number:"+4", type:"Comodin"});
+deck.push({color: null,number:"+4", type:"Comodin"});
+deck.push({color: null,number:"+4", type:"Comodin"});
+deck.push({color: null,number:"+4", type:"Comodin"});
+deck.push({color: null,number:"CC", type:"Comodin"});
+deck.push({color: null,number:"CC", type:"Comodin"});
+deck.push({color: null,number:"CC", type:"Comodin"});
+deck.push({color: null,number:"CC", type:"Comodin"});
 
 //console.log(JSON.stringify(deck));
 
 const players = rl.question("cuantos jugadores seran?",{})
 const totalCards = players * 7;
-// Repatir 7 cartas por jugador
-// Recorro el total de tarjetas ( total de tarjetas= tarjetas/7 * jugador)  
-// un arreglo x jugador  
-//  Diefrencia  de arreglo: uno se accede mediante posiciones y el objeto mediante nombre
-var  cardsPlayers = {};
+//repartir 7 cartas por jugador 
+// recorro al total de tarjetas (total de tarjetas por jugador = tarjetas/7 * jugador)
+//arreglo por cada jugador 
+var cardsPlayers ={};
 
-
-for(var cTCards = 0; cTCards < 7; cTCards++) {
-
-for(var cPlayers = 0; cPlayers < players; cPlayers++){
- 
-    if (!cardsPlayers[ "Player_"+cPlayers ] ) {
-        cardsPlayers[ "Player_"+cPlayers ] = [];
+for(var cTCards = 0; cTCards < 7; cTCards++){
+    for(var cPlayers = 0; cPlayers < players;cPlayers++){
+        if(!cardsPlayers["player"+cPlayers]){
+        cardsPlayers["player"+cPlayers] = [];
+        }
+      // console.log("players:", cPlayers);
+        cardsPlayers["player"+cPlayers].push(deck.shift());
+       //console.log(cardsPlayers)
     }
-    
-    cardsPlayers[ "Player_"+cPlayers ]. push(deck[cTCards]);
-    
+}
+//console.log(cardsPlayers)
+// npm i  <modulo>  o npm install <modulo> sirve para iniziaclizar un proyecto de node
+// la mesa es el arreglo
+var trash = [];
+
+trash.push(deck.shift());
+
+function cardValidation(card) {
+    // Obtener la primera carta de trash
+   console.log(card.number)
+    // if (card.numeber == 2) llamar a numero y carta 
+    if (card.number == trash[trash.length -1].number || card.color == trash[trash.length -1].color) { 
+        console.log("es igual");
+    } else {console.log("no es igual")}
 }
 
+for(const player in cardsPlayers) {
+    console.log(cardsPlayers[player])
+    // Primer player ponga una carta
+    // PASO 1: ver mis cartas
+    // PASO 2: Elegir cartas
+    // PASO 3 Validar Carta
+    // PASO 4 :Poner carta ( demano jugador trash)
+
+
 }
-console.log (cardsPlayers);
